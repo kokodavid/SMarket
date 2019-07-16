5f617c8fd55a1b7a2ff1c648e615bd027b2bd99a
kokodavid <kingkgdave@gmail.com>
2019-07-16 10:44:05 +0300

    continous development 3

--- lib/UserScreens/itemdetails.dart
+++ lib/UserScreens/itemdetails.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'cart.dart';
 
 class SmItemDetails extends StatefulWidget {
 
@@ -21,6 +24,7 @@ class SmItemDetails extends StatefulWidget {
 class _SmItemDetailsState extends State<SmItemDetails> {
   @override
   Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
 appBar: new AppBar(
   title: new Text("Item Details"),
@@ -40,9 +44,81 @@ appBar: new AppBar(
 
             )
             ),
+          ),
+          new Container(
+            height: 300.0,
+            decoration: new BoxDecoration(
+              color: Colors.grey.withAlpha(50),
+                    borderRadius: BorderRadius.only(
+                bottomRight: new Radius.circular(120.0),
+                bottomLeft: new Radius.circular(120.0)
+
+            )
+            ),
+          ),
+          new SingleChildScrollView(
+            child: new Column(
+              children: <Widget>[
+
+              ],
+            ),
           )
         ],
       ),
+      floatingActionButton: Stack(
+        alignment: Alignment.topLeft,
+        children: <Widget>[
+          new FloatingActionButton(
+            onPressed: (){
+              Navigator.of(context).push(new CupertinoPageRoute(
+                  builder: (BuildContext context) => new SMcart())
+              );}
+            ,child: new Icon(Icons.shopping_cart),
+
+          ),
+          new CircleAvatar(
+            radius:8.0,
+            backgroundColor: Colors.red,
+            child: new Text("0",
+                style:new TextStyle(color: Colors.white,fontSize: 12.0)),
+          ),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: new BottomAppBar(
+        color: Theme.of(context).primaryColor,
+        elevation: 0.0,
+        shape: new  CircularNotchedRectangle(),
+        notchMargin: 6.0,
+        child: new Container(
+          height: 50.0,
+          decoration: new BoxDecoration(color: Theme.of(context).primaryColor),
+          child: new Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              new Container(
+                width: (screenSize.width -20) / 2,
+                child: new Text(
+                    "ADD TO FAVOURITES",
+                textAlign: TextAlign.center,
+                style: new TextStyle(
+                  color: Colors.white, fontWeight: FontWeight.w700),
+                ),
+              ),
+              new Container(
+                width: (screenSize.width -20) / 2,
+                child: new Text(
+                  "ORDER NOW",
+                  textAlign: TextAlign.center,
+                  style: new TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.w700),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+
     );
   }
 }
